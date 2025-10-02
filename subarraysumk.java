edf58a5819afe61811997edd14ad49d31d3f4837
priyanka pareek <143818732+priyanka10111@users.noreply.github.com>
2025-10-02 16:19:43 +0530

    Update and rename subarray sum equals to k to subarraysumk.java

--- subarraysumk.java
+++ subarraysumk.java
@@ -1,20 +1,20 @@
-q560 subarray sum equals to k
-Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+//q560 subarray sum equals to k
+//Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
 
-A subarray is a contiguous non-empty sequence of elements within an array.
+//A subarray is a contiguous non-empty sequence of elements within an array.
 
  
 
-Example 1:
+//Example 1:
 
-Input: nums = [1,1,1], k = 2
-Output: 2
-Example 2:
+//Input: nums = [1,1,1], k = 2
+//Output: 2
+//Example 2:
 
-Input: nums = [1,2,3], k = 3
-Output: 2
+//Input: nums = [1,2,3], k = 3
+//Output: 2
  
-Solution:
+//Solution:
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int n = nums.length;
